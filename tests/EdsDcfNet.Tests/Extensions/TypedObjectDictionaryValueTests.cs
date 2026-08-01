2ff5d2bff7ef75279ba42f7668bfc003f14dafb1
Dietmar <2646931+dborgards@users.noreply.github.com>
2026-08-01 14:22:28 +0200

    test: cover all typed value conversion branches

--- tests/EdsDcfNet.Tests/Extensions/TypedObjectDictionaryValueTests.cs
+++ tests/EdsDcfNet.Tests/Extensions/TypedObjectDictionaryValueTests.cs
@@ -11,20 +11,33 @@ public class TypedObjectDictionaryValueTests
     public static TheoryData<string, ushort, object> TypedValues => new()
     {
         { "1", 0x0001, true },
+        { "true", 0x0001, true },
+        { "0", 0x0001, false },
+        { "false", 0x0001, false },
         { "0xFF", 0x0002, (sbyte)-1 },
+        { "017", 0x0002, (sbyte)15 },
+        { "0377", 0x0002, (sbyte)-1 },
         { "-1234", 0x0003, (short)-1234 },
         { "0xFFFFFFFF", 0x0004, -1 },
         { "255", 0x0005, (byte)255 },
+        { "0377", 0x0005, (byte)255 },
         { "0x1234", 0x0006, (ushort)0x1234 },
         { "0x12345678", 0x0007, 0x12345678U },
         { "1.25", 0x0008, 1.25F },
         { "Device name", 0x0009, "Device name" },
         { "Grüße", 0x000B, "Grüße" },
+        { "0xFF00FF", 0x0010, -65281 },
         { "-8388608", 0x0010, -8388608 },
         { "1.25", 0x0011, 1.25D },
         { "-549755813888", 0x0012, -549755813888L },
+        { "0xFFFFFFFFFFFF", 0x0013, -1L },
+        { "0xFFFFFFFFFFFFFF", 0x0014, -1L },
         { "9223372036854775807", 0x0015, long.MaxValue },
+        { "0xFFFFFFFFFFFFFFFF", 0x0015, -1L },
         { "16777215", 0x0016, 16777215U },
+        { "0xFFFFFFFFFF", 0x0018, 1099511627775UL },
+        { "281474976710655", 0x0019, 281474976710655UL },
+        { "0x1AB", 0x001A, 427UL },
         { "18446744073709551615", 0x001B, ulong.MaxValue }
     };
 
@@ -43,6 +56,113 @@ public class TypedObjectDictionaryValueTests
     {
         CanOpenValueConverter.Parse("0x0102FE", 0x000A)
             .Should().BeEquivalentTo(new byte[] { 0x01, 0x02, 0xFE });
+    }
+
+    [Fact]
+    public void Parse_ByteStringWithSeparators_IgnoresSpacesAndDashes()
+    {
+        CanOpenValueConverter.Parse("01 02-fe", 0x000F)
+            .Should().BeEquivalentTo(new byte[] { 0x01, 0x02, 0xFE });
+    }
+
+    [Theory]
+    [InlineData("0x123", 0x000A)]  // odd digit count
+    [InlineData("0xZZ", 0x000A)]   // invalid hex digit
+    [InlineData("maybe", 0x0001)]  // invalid boolean token
+    public void Parse_MalformedValue_ThrowsFormatException(string value, ushort dataType)
+    {
+        var act = () => CanOpenValueConverter.Parse(value, dataType);
+
+        act.Should().Throw<FormatException>();
+    }
+
+    [Theory]
+    [InlineData("256", 0x0005)]           // unsigned decimal overflow
+    [InlineData("0x1FF", 0x0005)]         // unsigned hex overflow
+    [InlineData("128", 0x0002)]           // signed decimal overflow
+    [InlineData("0x1000000", 0x0010)]     // signed hex overflow (24-bit)
+    public void Parse_ValueOutsideRange_ThrowsOverflowException(string value, ushort dataType)
+    {
+        var act = () => CanOpenValueConverter.Parse(value, dataType);
+
+        act.Should().Throw<OverflowException>();
+    }
+
+    [Theory]
+    [InlineData(0x000C)] // TIME_OF_DAY
+    [InlineData(0x000D)] // TIME_DIFFERENCE
+    public void ParseAndFormat_TimeTypes_ThrowNotSupportedException(ushort dataType)
+    {
+        var parse = () => CanOpenValueConverter.Parse("1", dataType);
+        var format = () => CanOpenValueConverter.Format(1, dataType);
+
+        parse.Should().Throw<NotSupportedException>().WithMessage($"*0x{dataType:X4}*");
+        format.Should().Throw<NotSupportedException>().WithMessage($"*0x{dataType:X4}*");
+    }
+
+    [Fact]
+    public void ParseAndFormat_NullValue_ThrowArgumentNullException()
+    {
+        var parse = () => CanOpenValueConverter.Parse(null!, 0x0005);
+        var format = () => CanOpenValueConverter.Format(null!, 0x0005);
+
+        parse.Should().Throw<ArgumentNullException>();
+        format.Should().Throw<ArgumentNullException>();
+    }
+
+    public static TheoryData<object, ushort, string> FormattedValues => new()
+    {
+        { false, 0x0001, "0" },
+        { (sbyte)-5, 0x0002, "-5" },
+        { (short)-1234, 0x0003, "-1234" },
+        { -70000, 0x0004, "-70000" },
+        { (byte)7, 0x0005, "7" },
+        { (ushort)512, 0x0006, "512" },
+        { 0x12345678U, 0x0007, "305419896" },
+        { "Device", 0x0009, "Device" },
+        { "Grüße", 0x000B, "Grüße" },
+        { -8388608, 0x0010, "-8388608" },
+        { 2.5D, 0x0011, "2.5" },
+        { -549755813888L, 0x0012, "-549755813888" },
+        { -140737488355328L, 0x0013, "-140737488355328" },
+        { -36028797018963968L, 0x0014, "-36028797018963968" },
+        { long.MinValue, 0x0015, "-9223372036854775808" },
+        { 16777215U, 0x0016, "16777215" },
+        { 1099511627775UL, 0x0018, "1099511627775" },
+        { 281474976710655UL, 0x0019, "281474976710655" },
+        { 72057594037927935UL, 0x001A, "72057594037927935" },
+        { ulong.MaxValue, 0x001B, "18446744073709551615" }
+    };
+
+    [Theory]
+    [MemberData(nameof(FormattedValues))]
+    public void Format_SupportedDataType_ProducesCanonicalString(object value, ushort dataType, string expected)
+    {
+        CanOpenValueConverter.Format(value, dataType).Should().Be(expected);
+    }
+
+    [Fact]
+    public void Format_SignedValueOutsideRange_ThrowsArgumentException()
+    {
+        var act = () => CanOpenValueConverter.Format(128, 0x0002);
+
+        act.Should().Throw<ArgumentException>().WithMessage("*0x0002*");
+    }
+
+    [Fact]
+    public void Format_NonByteArrayForOctetString_ThrowsArgumentException()
+    {
+        var act = () => CanOpenValueConverter.Format("not bytes", 0x000A);
+
+        act.Should().Throw<ArgumentException>().WithMessage("*0x000A*");
+    }
+
+    [Fact]
+    public void Format_UnsupportedDataType_ThrowsNotSupportedException()
+    {
+        var act = () => CanOpenValueConverter.Format(1, 0x0020);
+
+        act.Should().Throw<NotSupportedException>().WithMessage("*0x0020*");
     }
 
     [Fact]
@@ -229,6 +349,35 @@ public class TypedObjectDictionaryValueTests
         dictionary.GetParameterValueAsObject(0x9999).Should().BeNull();
         dictionary.GetParameterValueAsObject(0x3000).Should().BeNull();
         dictionary.SetParameterValue(0x9999, (object)1).Should().BeFalse();
+    }
+
+    [Fact]
+    public void TypedAccess_MissingSubObjectOrValue_ReturnsNullOrFalse()
+    {
+        var dictionary = CreateDictionary();
+        dictionary.Objects[0x1018].SubObjects[2] = new CanOpenSubObject { SubIndex = 2, DataType = 0x0005 };
+
+        dictionary.GetParameterValueAsObject(0x1018, 99).Should().BeNull();
+        dictionary.GetParameterValueAsObject(0x1018, 2).Should().BeNull();
+        dictionary.SetParameterValue(0x1018, 99, (object)1U).Should().BeFalse();
+    }
+
+    [Fact]
+    public void SetParameterValue_NullTypedValue_ThrowsArgumentNullException()
+    {
+        var dictionary = CreateDictionary();
+
+        var forObject = () => dictionary.SetParameterValue(0x2000, (object)null!);
+        var forSubObject = () => dictionary.SetParameterValue(0x1018, 1, (object)null!);
+
+        forObject.Should().Throw<ArgumentNullException>();
+        forSubObject.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Parse_NodeIdFormulaForSignedType_EvaluatesWithNodeId()
+    {
+        CanOpenValueConverter.Parse("$NODEID+0x100", 0x0004, nodeId: 2).Should().Be(0x102);
     }
 
     [Fact]
